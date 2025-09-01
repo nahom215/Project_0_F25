8590e19e8d8195016c46a43d17709ed26e380d60
nahom215 <tue83686@temple.edu>
2025-08-31 21:00:23 -0400

    final commit

--- random.c
+++ random.c
@@ -1,3 +1,20 @@
-//comment inital commit save the file.
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-def rand_string( num_words=1)
+// function to generate a random string of length `size`
+void rand_string(char *s, size_t size)
+{
+    static const char charset[] =
+        "abcdefghijklmnopqrstuvwxyz"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    if (size) {
+        for (size_t n = 0; n < size; n++) {
+            int key = rand() % (int)(sizeof charset - 1);
+            s[n] = charset[key];
+        }
+        s[size] = '\0'; // null terminator
+    }
+}
+
